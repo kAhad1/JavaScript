b861af0defa664ea0e4720f440ee5a4e67920fa8
Khaja Abdul Ahad <khajaahad2005@gmail.com>
2025-01-02 01:56:12 +0500

    Update promise.js

--- Practice/promise.js
+++ Practice/promise.js
@@ -1,21 +1,41 @@
 
-
+// Basic Promise
 // let promise= new Promise((accept,notAccept)=>{
 //     console.log("I am a Promise");
 //     notAccept("asd")
 // })
 
-const getData = (dataId, getNextData)=>{
-    return new Promise((resolve,reject)=>{
-    setTimeout(() => {
-        console.log("Data" , dataId);
-        reject("reject");
-        if(getNextData){
-            getNextData();
-        }
-    }, 5000)});
-}
 
-let abc=getData(2,()=>{
-    getData(3);
+// How to accept and reject Promise
+// const getData = (dataId, getNextData)=>{
+//     return new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         console.log("Data" , dataId);
+//         reject("reject");
+//         if(getNextData){
+//             getNextData();
+//         }
+//     }, 5000)});
+// }
+// let abc=getData(2,()=>{
+//     getData(3);
+// })
+
+
+const getPromise =()=>{
+    return new Promise((resolve,reject)=>{
+        console.log("I am a promise");
+        reject("error");
+        resolve("success");
+
+    })
+
+}
+let promise = getPromise();
+promise.then((res)=>{
+    console.log("Promise DOne",res);
+
+})
+promise.catch((err)=>{
+console.log("Promise failed",err);
 })
