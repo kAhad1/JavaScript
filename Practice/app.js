4f1889d1833928a7ea1fd0ea128b4ea0eb5d1cbd
Khaja Abdul Ahad <khajaahad2005@gmail.com>
2025-01-02 00:37:14 +0500

    commit

---
+++ Practice/app.js
@@ -0,0 +1,18 @@
+const getData = (dataId,getNextData) =>{
+    setTimeout(()=>{
+        console.log("Data",dataId);
+        if(getNextData){
+            getNextData();
+        }
+
+    },2000)
+}
+getData(1,()=>{
+    getData(2,()=>{
+        getData(3,()=>{
+            getData(4)
+        })
+    })
+})
+
+
